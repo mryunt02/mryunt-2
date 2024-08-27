7e7d567a1cf6ee32c9dddde893e8a7f2c28a0031
mryunt02 <mrthresh58@gmail.com>
2024-08-28 01:07:47 +0300

    buttons changed

--- components/button-link.tsx
+++ components/button-link.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 export function ButtonLink() {
   return (
     <>
-      <Button variant='link'>About Me</Button>
+      <Button>About Me</Button>
       <Button variant='link'>Projects</Button>
       <Button variant='link'>Experience</Button>
     </>
