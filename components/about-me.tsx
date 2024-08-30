2b5bdcc8896b175093796124a5dacfd49655d5c2
mryunt02 <mrthresh58@gmail.com>
2024-08-31 00:55:04 +0300

    about me ui responsive

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -14,20 +14,20 @@ const AboutMe = () => {
   return (
     <Card className='flex justify-between'>
       <div>
-        <CardHeader className='px-8 pt-8'>
+        <CardHeader className=' pl-4 pt-4  sm:pl-8 sm:pt-8'>
           <h1 className=' text-lg sm:text-4xl'>
             Hi, 👋 I'm <span className='text-[#D7721D]'>Buğrahan</span>
-            <i className='text-sm block'> Front-end Developer</i>
+            <i className=' text-xs sm:text-sm block'> Front-end Developer</i>
           </h1>
         </CardHeader>
-        <CardContent className='px-8'>
+        <CardContent className=' pl-4 sm:px-8'>
           <p>Card Content</p>
         </CardContent>
-        <CardFooter className='px-8'>
+        <CardFooter className='pl-4 px-8'>
           <p>Card Footer</p>
         </CardFooter>
       </div>
-      <div className='p-8'>
+      <div className='py-8 pr-8 absolute max-w-[200px] right-0 top-[-23px] sm:static sm:max-w-[1000px]'>
         <Image src={mryunt} alt='mryunt-photo' />
       </div>
     </Card>
