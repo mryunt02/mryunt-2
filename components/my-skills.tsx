1d465b58bf73ce484551ea839c85634944c9fa85
mryunt02 <mrthresh58@gmail.com>
2024-09-01 00:21:36 +0300

    test

--- components/my-skills.tsx
+++ components/my-skills.tsx
@@ -5,7 +5,9 @@ const MySkills = () => {
   return (
     <Card className='mt-3'>
       <CardHeader className=' pl-4 pt-4 pb-0 sm:pl-8 sm:pt-8 sm:pb-8'>
-        <h1 className=' text-lg sm:text-4xl  pt-3 sm:static sm:pt-0'>Skills</h1>
+        <h1 className=' text-lg sm:text-4xl  pt-3 sm:static sm:pt-0 text-[#D7721D]'>
+          Skills
+        </h1>
       </CardHeader>
       <CardContent className=' pl-4 sm:px-8 pb-8 flex flex-col gap-2'>
         ewqeqwe
