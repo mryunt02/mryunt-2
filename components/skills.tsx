f08d0f291fb33db384bff337dc41144665286541
mryunt02 <mrthresh58@gmail.com>
2024-09-01 00:57:18 +0300

    icons

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,42 +1,8 @@
 import React from 'react';
 import { Card, CardHeader } from './ui/card';
-import { reacticon } from '../icons/react-original-wordmark.svg';
 import Image from 'next/image';
 
-const skillIcons = [
-  {
-    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg',
-    alt: 'react-icon',
-  },
-  {
-    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg',
-    alt: 'js-icon',
-  },
-  {
-    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg',
-    alt: 'ts-icon',
-  },
-  {
-    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg',
-    alt: 'php-icon',
-  },
-  {
-    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg',
-    alt: 'java-icon',
-  },
-  {
-    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg',
-    alt: 'css-icon',
-  },
-  {
-    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg',
-    alt: 'html-icon',
-  },
-  {
-    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg',
-    alt: 'sass-icon',
-  },
-];
+import { skillIcons } from '@/skill-icons';
 
 const Skills = () => {
   return (
---
+++ skill-icons.ts
@@ -0,0 +1,62 @@
+export const skillIcons = [
+  {
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg',
+    alt: 'react-icon',
+  },
+  {
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg',
+    alt: 'js-icon',
+  },
+  {
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg',
+    alt: 'ts-icon',
+  },
+  {
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg',
+    alt: 'php-icon',
+  },
+  {
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg',
+    alt: 'java-icon',
+  },
+  {
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg',
+    alt: 'css-icon',
+  },
+  {
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg',
+    alt: 'html-icon',
+  },
+  {
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg',
+    alt: 'sass-icon',
+  },
+  {
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg',
+    alt: 'redux-icon',
+  },
+  {
+    src: 'https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg',
+    alt: 'twind-icon',
+  },
+  {
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg',
+    alt: 'node-icon',
+  },
+  {
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg',
+    alt: 'xpress-icon',
+  },
+  {
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg',
+    alt: 'mongo-db',
+  },
+  {
+    src: 'https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg',
+    alt: 'jest-icon',
+  },
+  {
+    src: 'https://cdn.worldvectorlogo.com/logos/nextjs-2.svg',
+    alt: 'next-icon',
+  },
+];
