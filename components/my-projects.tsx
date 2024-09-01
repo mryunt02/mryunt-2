ab6802093929589330154030c54f05a79ecb9740
mryunt02 <mrthresh58@gmail.com>
2024-09-02 02:31:10 +0300

    project.tsx added

--- components/my-projects.tsx
+++ components/my-projects.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Card, CardContent, CardHeader } from './ui/card';
+import mryunt from '../icons/mryunt.png';
+import Image from 'next/image';
 
 const MyProjects = () => {
   return (
