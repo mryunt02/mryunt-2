a58d627ae3edcaeb68981ef2c1abbce842453de6
mryunt02 <mrthresh58@gmail.com>
2024-09-02 03:09:18 +0300

    link to project added

--- components/project.tsx
+++ components/project.tsx
@@ -12,6 +12,8 @@ import mryunt from '../icons/mryunt.png';
 import weatherappimg from '../icons/weatherapp.png';
 import { Badge } from './ui/badge';
 import { projects } from '@/projects';
+import { ExternalLink } from 'lucide-react';
+import Link from 'next/link';
 
 export function Project() {
   return (
@@ -19,7 +21,12 @@ export function Project() {
       {projects.map((project) => (
         <Card className='p-5 flex flex-col gap-3' key={project.name}>
           <Image src={weatherappimg} alt='weather-app' />
-          <h2 className='text-xl'>{project.name}</h2>
+          <h2 className='text-xl flex items-center justify-between'>
+            {project.name}{' '}
+            <Link href='https://mryunt02.github.io/weatherapp' target='blank'>
+              <ExternalLink />
+            </Link>
+          </h2>
           <p>{project.description}</p>
           <div className='flex gap-2'>
             <p>Used Tools: </p>
