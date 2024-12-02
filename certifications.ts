39b3eef1a08937f7e1cf4288588171c0301af3c2
mryunt02 <mrthresh58@gmail.com>
2024-12-03 02:11:43 +0300

    certificates added

--- certifications.ts
+++ certifications.ts
@@ -1,5 +1,7 @@
 import { StaticImageData } from 'next/image';
-import jotformicon from './icons/jotform-icon.png';
+import recodedIcon from 'icons/re_coded_logo.jpeg';
+import scrimbaLogo from 'icons/scrimba_logo.jpeg';
+import freecodecamplogo from 'icons/free_code_camp_logo.jpeg';
 interface certificates {
   logo: StaticImageData;
   title: string;
@@ -10,11 +12,35 @@ interface certificates {
 }
 export const certificates: certificates[] = [
   {
-    logo: jotformicon,
+    logo: recodedIcon,
     title: 'AI for Fullstack Developers | Skill:Up',
     company: 'Jotform',
     date: 'Dec 2024',
     credentialURL: 'https://learn.re-coded.com/certificates/kkxxmfmd4h',
     skills: null,
+  },
+  {
+    logo: scrimbaLogo,
+    title: 'TypeScript',
+    company: 'Scrimba',
+    date: 'Dec 2024',
+    credentialURL: 'https://scrimba.com/certificates/cert-4b4b4b4b',
+    skills: ['TypeScript'],
+  },
+  {
+    logo: scrimbaLogo,
+    title: 'Advanced React',
+    company: 'Scrimba',
+    date: 'Dec 2024',
+    credentialURL: '',
+    skills: ['React'],
+  },
+  {
+    logo: freecodecamplogo,
+    title: 'JavaScript Algorithms and Data Structures',
+    company: 'freeCodeCamp',
+    date: 'Feb 2024',
+    credentialURL: '',
+    skills: ['JavaScript'],
   },
 ];
